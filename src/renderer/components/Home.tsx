c15095a44b687bee2aef8491ef0c0ecd8e3f9dfd
Drasenix <cauneauantoine@gmail.com>
2026-02-23 19:08:48 +0100

    feat: mise en place d'une page d'acceuil

---
+++ src/renderer/components/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  const intituleChoix1: string = "J'écris mon pattern";
+  const intituleChoix2: string = "Je pars d'un exemple";
+  return (
+    <div>
+      <h1>{intituleChoix1}</h1>
+      <h1>{intituleChoix2}</h1>
+    </div>
+  );
+}
