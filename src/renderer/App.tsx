3bcf866e18c477fe86676ef64a06b4e106e7269a
Drasenix <cauneauantoine@gmail.com>
2026-02-23 19:21:17 +0100

    feat: mise en place des différentes pages et du routing

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,18 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Choix from './components/Choix';
+import Pattern from './components/Pattern';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/choix" element={<Choix />} />
+        <Route path="/pattern" element={<Pattern />} />
       </Routes>
     </Router>
   );
--- src/renderer/components/Home.tsx
+++ src/renderer/components/Home.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   const intituleChoix1: string = "J'écris mon pattern";
   const intituleChoix2: string = "Je pars d'un exemple";
   return (
     <div>
-      <h1>{intituleChoix1}</h1>
-      <h1>{intituleChoix2}</h1>
+      <ul>
+        <h1>
+          <Link to="/choix">{intituleChoix2}</Link>
+        </h1>
+        <h1>
+          <Link to="/pattern">{intituleChoix1}</Link>
+        </h1>
+      </ul>
     </div>
   );
 }
