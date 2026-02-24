5b2a37e5ff256c02219ed626d27ff5a7e948c440
Drasenix <cauneauantoine@gmail.com>
2026-02-24 19:24:50 +0100

    feat: premiere lecture de son avec Tone js

--- src/services/features/audio/audio-service.ts
+++ src/services/features/audio/audio-service.ts
@@ -1,3 +1,8 @@
-export default function playSentence(sentence: string): void {
+export default async function playSentence(sentence: string): Promise<void> {
   console.log(sentence);
+  const Tone = await import('tone');
+  const player = new Tone.Player(
+    'https://tonejs.github.io/audio/berklee/gong_1.mp3',
+  ).toDestination();
+  player.autostart = true;
 }
