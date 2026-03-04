7b41d5559579c8038d30a4a6651198ade88d2dd2
Drasenix <cauneauantoine@gmail.com>
2026-03-04 16:24:16 +0100

    test: pattern adapter

---
+++ src/renderer/features/pattern/tests/pattern-adapter.test.ts
@@ -0,0 +1,34 @@
+import { PatternDB } from '../../../../main/db/models/pattern-db';
+import adaptPatterns from '../models/pattern-adapter';
+import { Pattern } from '../models/pattern-model';
+describe('adaptPatterns', () => {
+  it('should adapt patterns correctly', () => {
+    // Given
+    const pattern1: PatternDB = {
+      id: 'drum-and-bass',
+      name: 'drum and bass',
+      sentence: 'P Ts K P Ts K P',
+    };
+    const pattern2: PatternDB = {
+      id: 'dubstep',
+      name: 'dubstep',
+      sentence: 'P (Ts P) Ts P K (Ts P) Ts P K Ts',
+    };
+    const tested: PatternDB[] = [pattern1, pattern2];
+    // When
+    const result: Pattern[] = adaptPatterns(tested);
+    // Then
+    const pattern1Expected: Pattern = {
+      id: 'drum-and-bass',
+      name: 'drum and bass',
+      sentence: 'P Ts K P Ts K P',
+    };
+    const pattern2Expected: Pattern = {
+      id: 'dubstep',
+      name: 'dubstep',
+      sentence: 'P (Ts P) Ts P K (Ts P) Ts P K Ts',
+    };
+    const expected: Pattern[] = [pattern1Expected, pattern2Expected];
+    expect(result).toEqual(expected);
+  });
+});
