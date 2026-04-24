224b8ade7fa00906507a8d28d1002e8e82793d92
Drasenix <cauneauantoine@gmail.com>
2026-04-24 21:43:56 +0200

    feat: mise en place driver js

--- src/renderer/features/guide/components/GuideScreen.tsx
+++ src/renderer/features/guide/components/GuideScreen.tsx
@@ -1,17 +1,8 @@
-import GuideSection from './GuideSection';
-import { guideContent } from '../data/guide-content';
-
 export default function GuideScreen() {
   return (
     <div className="content-page">
       <div className="daw-layout">
-        <div className="daw-main">
-          <div className="flex flex-col gap-6">
-            {guideContent.map((section) => (
-              <GuideSection key={section.id} section={section} />
-            ))}
-          </div>
-        </div>
+        <div className="daw-main" />
       </div>
     </div>
   );
