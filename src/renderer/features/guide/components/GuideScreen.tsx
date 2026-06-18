d96cd1b1667cb203f4705fddc453ff6dada00b06
Drasenix <cauneauantoine@gmail.com>
2026-06-18 21:19:50 +0200

    feat: page guide cohérente

--- src/renderer/features/guide/components/GuideScreen.tsx
+++ src/renderer/features/guide/components/GuideScreen.tsx
@@ -3,11 +3,7 @@ import GuideContent from './GuideContent';
 export default function GuideScreen() {
   return (
     <div className="content-page">
-      <div className="daw-layout">
-        <div className="daw-main">
-          <GuideContent />
-        </div>
-      </div>
+      <GuideContent />
     </div>
   );
 }
